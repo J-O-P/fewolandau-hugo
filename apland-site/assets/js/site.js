a3503ba1b94e35033b36b5cda132af466d3371f4
Jens-Ole Petersen <jens-ole@petersen.top>
2024-11-30 23:06:17 +0100

    Make Google icons and materializeCSS locally available

--- apland-site/assets/js/site.js
+++ apland-site/assets/js/site.js
@@ -5,22 +5,22 @@
 // });
 
 document.addEventListener('DOMContentLoaded', function () {
-    const parallaxEffect = document.querySelectorAll('.parallax');
-    M.Parallax.init(parallaxEffect, {});
+	const parallaxEffect = document.querySelectorAll('.parallax');
+	M.Parallax.init(parallaxEffect, {});
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    const elems = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(elems, { edge: "right" });
+	const elems = document.querySelectorAll('.sidenav');
+	M.Sidenav.init(elems, { edge: 'right' });
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    const dropdowns = document.querySelectorAll('.dropdown-trigger');
-    M.Dropdown.init(dropdowns, {
-        hover: true,
-        coverTrigger: false,
-        constrainWidth: false
-    });
+	const dropdowns = document.querySelectorAll('.dropdown-trigger');
+	M.Dropdown.init(dropdowns, {
+		hover: true,
+		coverTrigger: false,
+		constrainWidth: false,
+	});
 });
 
 // document.addEventListener('DOMContentLoaded', function () {
@@ -32,8 +32,8 @@ document.addEventListener('DOMContentLoaded', function () {
 // });
 
 document.addEventListener('DOMContentLoaded', function () {
-    const elems = document.querySelectorAll('.materialboxed');
-    M.Materialbox.init(elems, {});
+	const elems = document.querySelectorAll('.materialboxed');
+	M.Materialbox.init(elems, {});
 });
 
 // $(() => {
@@ -43,5 +43,3 @@ document.addEventListener('DOMContentLoaded', function () {
 // $(() => {
 //     $("#includeNavigation").load("navigation.html");
 // });
-
-
--- apland-site/content/buchung/_index.md
+++ apland-site/content/buchung/_index.md
@@ -4,5 +4,3 @@ date: 2024-01-20T20:44:26+01:00
 draft: false
 weight: 100
 ---
-
-Hier können Sie uns direkt buchen.
